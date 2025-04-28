0f77568da3a6c9852b397476b9f005212041bfd3
ArthurRCL <arthur.rodriguescl@aluno.ueg.br>
2025-04-28 00:38:39 -0300

    Projeto ProgWeb

---
+++ src/test/java/com/progWeb/spring/progWeb/ProgWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progWeb.spring.progWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
